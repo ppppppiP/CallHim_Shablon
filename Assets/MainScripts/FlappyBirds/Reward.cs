abf9c379c06660aaf4cb145ebec7c5bed2747bcb
Поповичев Артём Алексеевич <67004012+ppppppiP@users.noreply.github.com>
2024-11-15 00:51:32 +0300

    ыфва

--- Assets/MainScripts/FlappyBirds/Reward.cs
+++ Assets/MainScripts/FlappyBirds/Reward.cs
@@ -1,6 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
+using YG;
+using YG.Example;
 
 public class Reward : MonoBehaviour
 {
+    public UnityEvent EOnShowAdd;
+    private void OnEnable()
+    {
+        YandexGame.RewardVideoEvent += OnShowAdd;
+    }
 
+    // Отписываемся от события открытия рекламы в OnDisable
+    private void OnDisable()
+    {
+        YandexGame.RewardVideoEvent -= OnShowAdd;
+    }
+    public void OnShowAdd(int id)
+    {
+        EOnShowAdd?.Invoke();
+    }
+
+    public void Show()
+    {
+        YandexGame.RewVideoShow(0);
+    }
 }
--- Assets/MainScripts/LoadVideoButtonClick.cs
+++ Assets/MainScripts/LoadVideoButtonClick.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 using Zenject;
 
@@ -7,7 +9,10 @@ public class LoadVideoButtonClick: MonoBehaviour
     private Button _button;
     [SerializeField] int Price;
     [SerializeField] GameObject Video;
-    bool isSold;
+    [SerializeField] GameObject Lock;
+    [SerializeField] TextMeshProUGUI text;
+    public UnityEvent OnPlay;
+    public bool isSold;
 
     [Inject] Scores _scores;
     [Inject] Reward _rew;
@@ -16,6 +21,15 @@ public class LoadVideoButtonClick: MonoBehaviour
     {
         _button = GetComponent<Button>();
         _button.onClick.AddListener(OpenVide);
+
+        if(text != null)
+        {
+            text.text = Price.ToString();
+        }
+        if(isSold == true)
+        {
+            Lock.SetActive(false);
+        }
     }
 
     public void OpenVide()
@@ -24,12 +38,16 @@ public class LoadVideoButtonClick: MonoBehaviour
         {
             _scores.AddScores(-Price);
             Video.SetActive(true);
+            isSold = true;
+            Lock.SetActive(false);
+            OnPlay?.Invoke();
         }
         else
         {
             _rew.gameObject.SetActive(true);
 
         }
-
+       
     }
 }
+
