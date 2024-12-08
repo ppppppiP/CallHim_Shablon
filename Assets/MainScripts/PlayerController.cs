067e3d950e1bd055e1426d948de89c0a696dc668
Hml <147100976+fibonacci111@users.noreply.github.com>
2024-12-08 14:00:09 +0300

    ваи
    вып

--- Assets/MainScripts/PlayerController.cs
+++ Assets/MainScripts/PlayerController.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UniversalMobileController;
+using YG;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,11 +11,7 @@ public class PlayerController : MonoBehaviour
     public float crouchHeight = 1f;
     public float climbSpeed = 3f;
     public Transform cameraTransform;
-    public float headClearance = 0.5f;
-    public float characterHeight = 2f;
-    public float gravity = -9.81f;
-    private float jumpGravityMultiplier = 1.5f;
-    private float fallSpeed = 2f;
+
     private CharacterController controller;
     private Vector3 velocity;
     private bool isGrounded;
@@ -22,100 +20,71 @@ public class PlayerController : MonoBehaviour
     private float currentSpeed;
     private bool isCrouching = false;
     private bool isClimbing = false;
-    public static PlayerController instance;
-    //public float climbAngleThreshold = 30f;
+    public float Charectereight = 2f;
+    float horizontal, vertical;
+    [SerializeField] FloatingJoyStick joyStick;
 
+    public static PlayerController instance;
     void Start()
     {
+        instance = this;
         controller = GetComponent<CharacterController>();
-        Cursor.lockState = CursorLockMode.Locked;
+        if (YandexGame.EnvironmentData.isDesktop)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
         currentSpeed = speed;
     }
 
     void Update()
     {
-        isGrounded = controller.isGrounded;
         if (isClimbing)
         {
             HandleClimbing();
         }
         else
         {
-            HandleMovement();
-            if (isGrounded && Input.GetButtonDown("Jump"))
+            if (YandexGame.EnvironmentData.isDesktop)
+            {
+                horizontal = Input.GetAxis("Horizontal");
+                vertical = Input.GetAxis("Vertical");
+            }
+            else
+            {
+                horizontal = joyStick.GetHorizontalValue();
+                vertical = joyStick.GetVerticalValue();
+            }
+
+
+            Vector3 move = (transform.forward * vertical + transform.right * horizontal) * currentSpeed;
+
+            if (Input.GetKeyDown(KeyCode.LeftControl))
+            {
+                isCrouching = !isCrouching;
+                currentSpeed = isCrouching ? crouchSpeed : speed;
+                controller.height = isCrouching ? crouchHeight : Charectereight;
+            }
+
+            if (Input.GetButtonDown("Jump"))
             {
                 Jump();
             }
-            ApplyGravity();
+
+            velocity.y += Physics.gravity.y * Time.deltaTime;
+            controller.Move(move * Time.deltaTime + velocity * Time.deltaTime);
+            isGrounded = controller.isGrounded;
         }
-        HandleCameraRotation();
+
+        // HandleCameraRotation();
     }
 
-    private void HandleMovement()
+    public void Jump()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector3 move = transform.right * horizontal + transform.forward * vertical;
-
-        if (Input.GetKeyDown(KeyCode.LeftControl))
+        if (isGrounded)
         {
-            if (isCrouching && !IsOverheadObstructed())
-            {
-                StandUp();
-            }
-            else if (!IsOverheadObstructed())
-            {
-                Crouch();
-            }
-        }
-        controller.Move(move * currentSpeed * Time.deltaTime);
-    }
-
-    private void Jump()
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, 1.5f))
-        {
-            //float surfaceAngle = Vector3.Angle(hit.normal, Vector3.up);
-            //if (surfaceAngle <= climbAngleThreshold)
-            //{
             velocity.y = jumpForce;
-            //}
         }
-    }
-
-    private void ApplyGravity()
-    {
-        if (isGrounded && velocity.y < 0)
-        {
-            velocity.y = -fallSpeed;
-        }
-        else
-        {
-            float gravityMultiplier = isGrounded ? 1 : jumpGravityMultiplier;
-            velocity.y += gravity * gravityMultiplier * Time.deltaTime;
-        }
-        controller.Move(velocity * Time.deltaTime);
-    }
-
-    private void Crouch()
-    {
-        isCrouching = true;
-        currentSpeed = crouchSpeed;
-        controller.height = crouchHeight;
-    }
-
-    private void StandUp()
-    {
-        isCrouching = false;
-        currentSpeed = speed;
-        controller.height = characterHeight;
-    }
-    private bool IsOverheadObstructed()
-    {
-        Vector3 rayOrigin = transform.position + Vector3.up * (controller.height);
-        RaycastHit hit;
-        return Physics.Raycast(rayOrigin, Vector3.up, out hit, headClearance);
     }
 
     private void HandleClimbing()
@@ -129,16 +98,19 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void HandleCameraRotation()
-    {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+    //private void HandleCameraRotation()
+    //{
+    //    float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+    //    float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
-xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-        yRotation += mouseX; cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        transform.rotation = Quaternion.Euler(0f, yRotation, 0f);
-    }
+    //    xRotation -= mouseY;
+    //    xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+    //    yRotation += mouseX;
+
+    //    cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    //    transform.rotation = Quaternion.Euler(0f, yRotation, 0f);
+    //}
+
     //private void OnTriggerEnter(Collider other)
     //{
     //    if (other.CompareTag("Ladder"))
